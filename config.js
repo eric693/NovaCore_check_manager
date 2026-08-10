4156743085b3c69fcff20ff17d24fcfb22dbf63f
eric693 <exo881222@gmail.com>
2026-08-10 15:22:27 +0000

    update

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 
 const API_CONFIG = {
   // 正式環境的 API URL
-  apiUrl: "https://script.google.com/macros/s/AKfycbyaaWuDcylPEDfu41_Kz0_D-IdcSxbRXhUgHCRVOBR9vgNxRI6Nu5vUT6gZG21i6nRzcA/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwIMyrk-weBQCgx7AdZFZmlq9ornv_bTUAJjbH7NUd1nBxwTqxQQpJh5Af3znqcpqUW/exec",
   
   // 新增回呼網址
-  redirectUrl: "https://eric693.github.io/JiaChun_check_manager/"
+  redirectUrl: "https://eric693.github.io/Linling_check_manager/"
   // 你也可以在這裡加入其他設定，例如：
   // timeout: 5000,
   // version: 'v4.6.0'
