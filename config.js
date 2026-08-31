2664bbfa745410e1e16606768fd30f48b59a194e
eric693 <exo881222@gmail.com>
2026-08-31 11:02:19 +0000

    update

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 
 const API_CONFIG = {
   // 正式環境的 API URL
-  apiUrl: "https://script.google.com/macros/s/AKfycbw993ETQYXh-HctgGMNvDnBZFjlNRFTcOSy1lqNVwDIZwSn6GGFJyCrXpbSR0BTg7hHkw/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxsbB923Kjg-X_C7kFM4e552Gu4Nrhlcfafhl9Q2ITDx8U1fQLtQFX0RnVKd6jahuweTA/exec",
   
   // 新增回呼網址
-  redirectUrl: "https://eric693.github.io/yi-chun_check_manager/"
+  redirectUrl: "https://eric693.github.io/NovaCore_check_manager/"
   // 你也可以在這裡加入其他設定，例如：
   // timeout: 5000,
   // version: 'v4.6.0'
